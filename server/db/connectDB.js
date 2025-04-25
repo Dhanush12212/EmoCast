0a6530dea5a86542bcf069b930596ae00c8b3a31
Dhanaush12212 <dhanush12212@gmail.com>
2025-04-25 22:58:25 +0530

    Working on Backend

---
+++ server/db/connectDB.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+//MongoDB Connection
+const connectDB = async() => {
+    try {
+        await mongoose
+        .connect(process.env.MONGODB_URL);
+        console.log(JSON.stringify({ 
+            message: "Connected to MongoDB", 
+            status: "success" 
+        }));
+    } 
+    catch(error) {
+        console.error(
+            JSON.stringify({ 
+                message: "Failed to connect with DB", 
+                status: "error", 
+                error: error.message 
+            })
+        );
+        process.exit(1);  
+    };
+};
+
+export default connectDB;
