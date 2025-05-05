8bfd9a8beb3911373971ae1702b5dd31cc9fa3f1
Dhanaush12212 <dhanush12212@gmail.com>
2025-05-05 21:54:28 +0530

    Controller for RegisterUser

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,9 +1,54 @@
-import express from 'express';
+import jwt from 'jsonwebtoken';
+import { User } from '../models/user.model.js'; 
+import bcrypt from 'bcryptjs';
+import ApiError from '../utils/ApiError.utils.js';
+import ApiResponse from '../utils/ApiResponse.utils.js';
+import asyncHandler from '../utils/asyncHandler.utils.js';
 
 
-const registerUser = () => {
+const registerUser = asyncHandler(async (req, res) => {
+    let { username, password, email } = req.body;
 
-};
+    //validate user
+    if( !username || !password || !email )
+        throw new ApiError( 400, "All fields are required!" );
+
+    const existingUser = await User.findOne({ email });
+    if(existingUser)
+        throw new ApiError( 400, "User already found!" );
+
+    //Salting
+    const salt = await bcrypt.genSalt(10);
+
+    //Haisng the password
+    const hashedPassword = await bcrypt.hash( password, salt);
+
+    //Creating new User
+    const newUser = await User.create({
+        username,
+        password: hashedPassword,
+        email,
+    })
+
+    //Token Generation
+    const token = jwt.sign({ email: newUser.email, id: newUser._id },
+        process.env.JWT_SECRET,
+        { expiresIn: "12h" }
+    );
+
+    //Cookie Storage
+    res.cookie( "token", token, {
+        //Prevent JS access
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "Production" ? "None" : "Lax", 
+    });
+
+    const { password: _, ...userWithoutPassword } = newUser._doc;
+
+    return res.status(201).send( new ApiResponse( 201, { user: userWithoutPassword }, "Successfully Registered.")) 
+});
+
 
 const loginUser = () => {
 
@@ -17,7 +62,6 @@ const logoutUser = () => {
 const checkLog = () => {
 
 };
-
 
 export {
     registerUser,
